d3e2958b216222938d4874c626f6ac1c90f2b369
noobiba <noobanine@gmail.com>
2018-10-09 13:30:33 +0200

    Add change of label description after click on button.

--- first-swift-app/ViewController.swift
+++ first-swift-app/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.label.text = "Bitte Button drücken!"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
