156542b5b78568f77076c5ddab0efd317b6929f8
noobiba <noobanine@gmail.com>
2018-10-09 11:48:59 +0200

    Add files to remote repo.

---
+++ first-swift-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  first-swift-app
+//
+//  Created by noobiba on 09.10.18.
+//  Copyright © 2018 noobiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
