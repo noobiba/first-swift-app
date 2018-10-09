eba3b5447305845a6a2ac4d4afca42962b539419
noobiba <noobanine@gmail.com>
2018-10-09 13:27:06 +0200

    Align button to label. Add action (Hallo Welt) to button click.

--- first-swift-app/ViewController.swift
+++ first-swift-app/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func button(_ sender: Any) {
+        self.label.text = "Hallo Welt"
+    }
 }
 
